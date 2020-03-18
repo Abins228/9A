df9ed3ade2f4ea27bf267ed5f1f7cda464ac6984
Макс <maxmex2004@mail.ru>
2020-03-18 23:39:35 +0300

    Реализовали Back

--- app/src/main/java/com/example/myapplication/Catalog_Activity.java
+++ app/src/main/java/com/example/myapplication/Catalog_Activity.java
@@ -14,7 +14,7 @@ public class Catalog_Activity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catalog);
+        setContentView(R.layout.activity_main);
         bt=findViewById(R.id.back);
         bt.setOnClickListener(this);
     }
