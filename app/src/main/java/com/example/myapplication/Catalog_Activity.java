6258bd6f3b1842212c168099f46fc67f2895e323
Макс <maxmex2004@mail.ru>
2020-03-18 23:29:56 +0300

    Реализовали Back

--- app/src/main/java/com/example/myapplication/Catalog_Activity.java
+++ app/src/main/java/com/example/myapplication/Catalog_Activity.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Catalog_Activity extends AppCompatActivity {
+public class Catalog_Activity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catalog_);
-   }
+        setContentView(R.layout.activity_catalog);
+        bt=findViewById(R.id.back);
+        bt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(Catalog_Activity.this, Catalog_Activity.class);
+        startActivity(i);
+    }
 }
