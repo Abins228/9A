ad5bd9cd0264a8cfc67f2dfcf5e2e4580af73e05
Abins228 <62339412+Abins228@users.noreply.github.com>
2020-03-20 11:50:00 +0300

    1.9

--- app/src/main/java/com/example/myapplication/sborka_activity.java
+++ app/src/main/java/com/example/myapplication/sborka_activity.java
@@ -2,13 +2,25 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class sborka_activity extends AppCompatActivity {
+public class sborka_activity extends AppCompatActivity implements View.OnClickListener {
+    ImageButton bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sborka_activity);
+        bt = findViewById(R.id.back2);
+        bt.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        Intent s;
+        s = new Intent(sborka_activity.this, MainActivity.class);
+        startActivity(s);
     }
 }
