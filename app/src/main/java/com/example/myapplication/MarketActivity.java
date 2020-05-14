f0bf5145d3f9026b6595abf1d09b82a19e3bde87
Макс <maxmex2004@mail.ru>
2020-05-14 18:55:50 +0300

    Выводятся матери бп процы куллеры процов m2 hdd

--- app/src/main/java/com/example/myapplication/MarketActivity.java
+++ app/src/main/java/com/example/myapplication/MarketActivity.java
@@ -23,6 +23,7 @@ public class MarketActivity extends AppCompatActivity {
     ListView list;
     ListAdapter listAdapter;
     Selectable table;
+    TextView namemarket;
 
 
 
@@ -35,83 +36,83 @@ public class MarketActivity extends AppCompatActivity {
         dbServer = new DBServer(this);
 
         list = findViewById(R.id.listView);
+        namemarket = findViewById(R.id.namemarket);
 
         if (getIntent().hasExtra("MotherBoard")) {
             int value = getIntent().getIntExtra("MotherBoard", 1);
             if (value == BuildActivity.MOTHERBOARD) {
-                Toast.makeText(getApplicationContext(), "материнки", Toast.LENGTH_SHORT).show();
                 table = dbServer.new MotherboardsTable();
                 listAdapter = new ListAdapter<Motherboards>(this, table.selectAll());
+                namemarket.setText("Материнские платы");
             }
         }
         if (getIntent().hasExtra("Central processor")) {
             int value = getIntent().getIntExtra("Central processor", 2);
             if (value == BuildActivity.PROCES) {
-                Toast.makeText(getApplicationContext(), "Процы", Toast.LENGTH_SHORT).show();
                 table = dbServer.new CPUTable();
                 listAdapter = new ListAdapter<CPU>(this, table.selectAll());
+                namemarket.setText("Процессоры");
             }
         }
         if (getIntent().hasExtra("Cooler")) {
             int value = getIntent().getIntExtra("Cooler", 3);
             if (value == BuildActivity.COOLERS) {
-                Toast.makeText(getApplicationContext(), "кулера", Toast.LENGTH_SHORT).show();
             }
         }
         if (getIntent().hasExtra("Graphics card")) {
             int value = getIntent().getIntExtra("Graphics card", 4);
             if (value == BuildActivity.GPU) {
-                Toast.makeText(getApplicationContext(), "видюхи", Toast.LENGTH_SHORT).show();
+                namemarket.setText("Видеокарты");
             }
         }
         if (getIntent().hasExtra("Ram")) {
             int value = getIntent().getIntExtra("Ram", 5);
             if (value == BuildActivity.RAM) {
-                Toast.makeText(getApplicationContext(), "память", Toast.LENGTH_SHORT).show();
+                namemarket.setText("Оперативная память");
             }
         }
         if (getIntent().hasExtra("HDD")) {
             int value = getIntent().getIntExtra("HDD", 6);
             if (value == BuildActivity.HDD) {
-                Toast.makeText(getApplicationContext(), "жесткие", Toast.LENGTH_SHORT).show();
                 table = dbServer.new HDDTable();
                 listAdapter = new ListAdapter<HDD>(this, table.selectAll());
+                namemarket.setText("HDD диски");
             }
         }
         if (getIntent().hasExtra("SSD")) {
             int value = getIntent().getIntExtra("SSD", 7);
             if (value == BuildActivity.SSD) {
-                Toast.makeText(getApplicationContext(), "SSD", Toast.LENGTH_SHORT).show();
+                namemarket.setText("SSD диски");
             }
         }
         if (getIntent().hasExtra("M2")) {
             int value = getIntent().getIntExtra("M2", 8);
             if (value == BuildActivity.M2) {
-                Toast.makeText(getApplicationContext(), "M2", Toast.LENGTH_SHORT).show();
                 table = dbServer.new M2Table();
                 listAdapter = new ListAdapter<M2>(this, table.selectAll());
+                namemarket.setText("M2 накопители");
             }
         }
         if (getIntent().hasExtra("CPUCOOLER")) {
             int value = getIntent().getIntExtra("CPUCOOLER", 9);
             if (value == BuildActivity.CPUCOOLER) {
-                Toast.makeText(getApplicationContext(), "кулер проца", Toast.LENGTH_SHORT).show();
                 table = dbServer.new Coolers_CPUTable();
                 listAdapter = new ListAdapter<Coolers_CPU>(this, table.selectAll());
+                namemarket.setText("Кулеры для процессора");
             }
         }
         if (getIntent().hasExtra("BP")) {
             int value = getIntent().getIntExtra("BP", 10);
             if (value == BuildActivity.BP) {
-                Toast.makeText(getApplicationContext(), "блоки питания", Toast.LENGTH_SHORT).show();
                 table = dbServer.new BPTable();
                 listAdapter = new ListAdapter<BP>(this, table.selectAll());
+                namemarket.setText("Блоки питания");
             }
         }
         if (getIntent().hasExtra("BODY")) {
             int value = getIntent().getIntExtra("BODY", 11);
             if (value == BuildActivity.BODY) {
-                Toast.makeText(getApplicationContext(), "корпуса", Toast.LENGTH_SHORT).show();
+                namemarket.setText("Корпуса");
             }
 
 
